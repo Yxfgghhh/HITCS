bd26c46ca7b89852cb72efc784f4700456f2cf13
Yxfgghhh <2055916199@qq.com>
2026-08-24 17:36:22 +0800

    Delete 人工智能 directory

--- 人工智能/人工智能程序设计实践/K-means/1.py
+++
@@ -1,7 +0,0 @@
-import random
-import pandas as pd
-import numpy as np
-
-data = pd.read_csv('./Iris.csv').values
-characters = data[:,4]
-print(characters)
--- 人工智能/人工智能程序设计实践/Knn/1.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import random
-
-for i in range(150):
-    print (i)
